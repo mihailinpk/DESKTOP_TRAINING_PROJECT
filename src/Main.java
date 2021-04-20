32373ea50031c5aa2d6b2b8de5fb0abb549ad7a4
mihailinpk <49496789+mihailinpk@users.noreply.github.com>
2021-04-20 07:38:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Тестовый проект");
+        frame.setSize(600, 400);
+        frame.add(new MainForm().getMainPanel());
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+    }
+
+}
---
+++ src/MainForm.java
@@ -0,0 +1,148 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.beans.PropertyChangeListener;
+
+public class MainForm {
+
+    private JPanel mainPanel;
+    private JTextArea textArea;
+    private JButton countButton;
+    private JButton clearButton;
+
+    public MainForm() {
+
+        clearButton.addActionListener(new Action() {
+            @Override
+            public Object getValue(String key) {
+                return null;
+            }
+
+            @Override
+            public void putValue(String key, Object value) {
+
+            }
+
+            @Override
+            public void setEnabled(boolean b) {
+
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return false;
+            }
+
+            @Override
+            public void addPropertyChangeListener(PropertyChangeListener listener) {
+
+            }
+
+            @Override
+            public void removePropertyChangeListener(PropertyChangeListener listener) {
+
+            }
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textArea.setText("");
+            }
+        });
+
+        countButton.addActionListener(new Action() {
+            @Override
+            public Object getValue(String key) {
+                return null;
+            }
+
+            @Override
+            public void putValue(String key, Object value) {
+
+            }
+
+            @Override
+            public void setEnabled(boolean b) {
+
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return false;
+            }
+
+            @Override
+            public void addPropertyChangeListener(PropertyChangeListener listener) {
+
+            }
+
+            @Override
+            public void removePropertyChangeListener(PropertyChangeListener listener) {
+
+            }
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String value = textArea.getText();
+                int length = value.length();
+                JOptionPane.showMessageDialog(
+                    mainPanel,
+                    length + " символов",
+                    "Длина текста",
+                    JOptionPane.PLAIN_MESSAGE
+                );
+            }
+        });
+
+        textArea.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textArea.setBackground(Color.YELLOW);
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+
+        textArea.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                if (e.getKeyChar() == '6')  {
+                    textArea.setText(textArea.getText() + " шестерка");
+                }
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+
+    }
+
+    public JPanel getMainPanel()    {
+        return mainPanel;
+    }
+
+}
